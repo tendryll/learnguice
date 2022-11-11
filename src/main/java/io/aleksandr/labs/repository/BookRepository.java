22bd6be41c556ae6b1e03f5005fe20922a908668
Sean Alphonse <sean@aleksandr.cc>
2022-11-10 21:37:23 -0600

    * JavaDoc comments were added.

--- src/main/java/io/aleksandr/labs/repository/BookRepository.java
+++ src/main/java/io/aleksandr/labs/repository/BookRepository.java
@@ -5,13 +5,42 @@ import io.aleksandr.labs.model.Book;
 import java.util.List;
 
 public interface BookRepository {
+  /**
+   * Creates a book and adds it the library.
+   *
+   * @param book The book details to be saved.
+   * @return The book's ID.
+   */
   long create(final Book book);
 
+  /**
+   * Updates the book by ID.
+   *
+   * @param book The book details to be updated.
+   * @return The updated book if the book to be updated is found, otherwise -1 is returned.
+   */
   Book update(final Book book);
 
+  /**
+   * Retrieves a book.
+   *
+   * @param id The ID of the book to be found.
+   * @return The retrieved book if found, otherwise null.
+   */
   Book get(final long id);
 
+  /**
+   * Returns a list of books filtered by tag.
+   *
+   * @param tag The tag to find books by.
+   * @return A list of books.
+   */
   List<Book> find(final String tag);
 
+  /**
+   * The book to delete by ID.
+   *
+   * @param id The ID of the book to delete.
+   */
   void delete(final long id);
 }
